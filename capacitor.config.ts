b94f1957aa29551c74b17de392140091be992bac
Eric Horodyski <horodyski@ionic.io>
2024-08-16 14:10:21 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'vue-select-test',
+  webDir: 'dist'
+};
+
+export default config;
